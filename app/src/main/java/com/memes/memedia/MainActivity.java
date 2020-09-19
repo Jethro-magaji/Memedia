1621df460d6f693bae57eb097f6f9b571dde7aba
jethro-magaji <jemagaji38@gmail.com>
2020-09-19 16:03:24 +0100

    Added the ui and somes files, though the MainActivity.java needs some corrections.

--- app/src/main/java/com/memes/memedia/MainActivity.java
+++ app/src/main/java/com/memes/memedia/MainActivity.java
@@ -1,56 +1,53 @@
-package com.memes.memedia;
+//package com.memes.memedia
 
-import android.os.Bundle;
+//import android.content.Intent
+//import android.os.Bundle
+//import com.google.android.material.bottomnavigation.BottomNavigationView
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.fragment.app.Fragment
+//import com.memes.memedia.ui.home.HomeFragment
+//import com.memes.memedia.ui.profile.ProfileFragment
+//import com.memes.memedia.ui.search.SearchFragment
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+//class MainActivity : AppCompatActivity()  {
 
-import android.view.View;
+//    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home -> {
+//                    moveToFragment(HomeFragment())
+//                    return@OnNavigationItemSelectedListener true
+//                }
+//                R.id.search -> {
+//                    moveToFragment(SearchFragment())
+//                    return@OnNavigationItemSelectedListener true
+//                }
+//                R.id.profile -> {
+//                    moveToFragment(ProfileFragment())
+//                    return@OnNavigationItemSelectedListener true
+//                }
+//            }
+//
+//            false
+//    }
+//
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+//
+//        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+//
+//
+//        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+//
+//        moveToFragment(HomeFragment())
+//    }
+//
+//    private fun moveToFragment(fragment: Fragment) {
+//        val fragmentTrans = supportFragmentManager.beginTransaction()
+//        fragmentTrans.replace(R.id.fragment_container, fragment)
+//        fragmentTrans.commit()
+//    }
 
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-}
+//}
