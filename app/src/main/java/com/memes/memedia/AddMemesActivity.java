e31fad8d25c05f67a6c19ab49853a28fa6ca12c1
jethro-magaji <jemagaji38@gmail.com>
2020-09-20 03:48:39 +0100

    Added activity_add_memes.xml and item_card.xml UI, and some implementations

---
+++ app/src/main/java/com/memes/memedia/AddMemesActivity.java
@@ -0,0 +1,14 @@
+package com.memes.memedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddMemesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_memes);
+    }
+}
